b70028449c7ee4f2bfa3b8f4d5e8903f4f5625d1
Mikhail Kuznetsov <78100892+DevMBS@users.noreply.github.com>
2021-10-26 20:06:04 +0300

    Optimized

--- qr.py
+++ qr.py
@@ -1,19 +1,16 @@
 import rospy
 import cv2 as cv
-from clover import srv
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from pyzbar.pyzbar import decode as qr_read
 rospy.init_node('flight')
-bridge = CvBridge()
 qr_debug = rospy.Publisher("/qr_debug", Image)
-def qr_check(data):
-    frame = bridge.imgmsg_to_cv2(data, 'bgr8')
-    barcodes = qr_read(frame)
+def qr(data):
+    barcodes = qr_read(CvBridge().imgmsg_to_cv2(data, 'bgr8'))
     if barcodes:
         f = open('qdata.txt', 'w')
         f.write(barcodes[0].data)
         f.close()
         image_sub.unregister()
-image_sub = rospy.Subscriber('main_camera/image_raw', Image, qr_check, queue_size=1)  # try to read qr
+image_sub = rospy.Subscriber('main_camera/image_raw', Image, qr, queue_size=1)
 rospy.sleep(4)
