12970fe813431d8ada6ae4fe39506ac8ea62d6fc
Mikhail Kuznetsov <78100892+DevMBS@users.noreply.github.com>
2021-10-26 20:02:32 +0300

    +comments

--- test.js
+++ test.js
@@ -1,13 +1,25 @@
+//library import
 const clover = require("./clover");
+//arm and navigate to 1.5m up
 clover.navigate(0, 0, 1.5, 0, 0, 0.5, 'body', true);
 clover.sleep(10);
+//set effect for led strip
 clover.led('rainbow');
+//get qr data
 clover.get_qr();
-console.log(`QR data: ${clover.qr()}`)
+//display it
+console.log(`QR data: ${clover.qr()}`);
+//get color data
 clover.get_color();
-console.log(`Color data: ${clover.color()}`)
+//display it
+console.log(`Color data: ${clover.color()}`);
+//get telemetry from the drone
 clover.get_telemetry();
-console.log(`Telemetry data: ${clover.telemetry()}`)
+//display it
+console.log(`Telemetry data: ${clover.telemetry()}`);
+//get data from the rangefinder
 clover.get_range();
-console.log(`Range data: ${clover.range()}`)
+//display it
+console.log(`Range data: ${clover.range()}`);
+//land
 clover.land();
