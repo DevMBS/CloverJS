ef6f5294e3397df67bd4ab34c21c2e4949de53f3
Mikhail Kuznetsov <78100892+DevMBS@users.noreply.github.com>
2021-10-26 20:18:50 +0300

    Delete range.py

--- range.py
+++
@@ -1,7 +0,0 @@
-import rospy
-from sensor_msgs.msg import Range
-rospy.init_node('flight')
-data = rospy.wait_for_message('rangefinder/range', Range)
-f = open('rdata.txt', 'w')
-f.write(str(data.range))
-f.close()
